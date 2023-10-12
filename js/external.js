d5b2a7b79367559b8ed69306a5c2a59c718584d0
John Barber <jgbarber1981@gmail.com>
2023-10-12 10:20:56 -0500

    update external.js

--- js/external.js
+++ js/external.js
@@ -1,1 +1,4 @@
-console.log("Hello from external JavaScript")
+console.log("Hello from external JavaScript");
+alert("Welcome to my Website!");
+let color = prompt("What is your favorite color?");
+alert(`Great, ${color} is my favorite color too!`);
