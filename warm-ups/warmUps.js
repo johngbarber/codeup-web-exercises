803e1c9e54655594b883e1b8112ef276665fa962
John Barber <jgbarber1981@gmail.com>
2023-10-19 09:41:22 -0500

    updated warmups.js file with daily warmup

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -20,22 +20,33 @@
 //     return capitalizedFirstLetter + restOfWord
 //
 // }
+//
+// const capitalizeFirstLetter = str => str.substring(0,1).toUpperCase() + str.substring(1);
+//
+// //function to compare three user provided numbers for which is the largest.
+// function largestNum () {
+//     //prompt user for first input
+//     let numOne = parseInt(prompt("Please enter your first number."));
+//     //prompt user for second input
+//     let numTwo = parseInt(prompt("Please enter your second number."));
+//     //prompt user for third input
+//     let numThree = parseInt(prompt("Please enter your third number."));
+//     //compare three user inputs using Math.max method
+//     let largestNum = Math.max(numOne, numTwo, numThree)
+//     //return result via an alert
+//     alert(`The largest number is ${largestNum}`)
+//
+//     }
+//
+// largestNum()
 
-const capitalizeFirstLetter = str => str.substring(0,1).toUpperCase() + str.substring(1);
-
-//function to compare three user provided numbers for which is the largest.
-function largestNum () {
-    //prompt user for first input
-    let numOne = parseInt(prompt("Please enter your first number."));
-    //prompt user for second input
-    let numTwo = parseInt(prompt("Please enter your second number."));
-    //prompt user for third input
-    let numThree = parseInt(prompt("Please enter your third number."));
-    //compare three user inputs using Math.max method
-    let largestNum = Math.max(numOne, numTwo, numThree)
-    //return result via an alert
-    alert(`The largest number is ${largestNum}`)
-
+// create a while loop that starts at num =100
+let num = 100
+do {
+    num -= 5
+    if (num % 2 === 0 && num !== 0) {
+        console.log(`${num} is even`);
+    } else {
+        console.log(num);
     }
-
-largestNum()
+} while (num > 0);
