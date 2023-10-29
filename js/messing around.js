0d6842dc77273293716f4c4245d2f5db5945447a
John Barber <jgbarber1981@gmail.com>
2023-10-29 04:16:45 -0500

    worked with destructuring in messing around.js

--- js/messing around.js
+++ js/messing around.js
@@ -179,3 +179,31 @@ function abTest(a, b) {
     }
     return a > b? a : b;
 }
+
+// for loop array iteration practice
+//Declare and initialize a variable total to 0. Use a for loop to add the value of each element of the myArr array to total.
+
+const myArr = [2,3,4,5,6];
+let total = 0;
+for (let i =0; i < myArr.length; i++){
+    total += myArr[i];
+}
+
+console.log(total);
+
+//We have defined a function named rangeOfNumbers with two parameters. The function should return an array of integers which begins with a number represented by the startNum parameter and ends with a number represented by the endNum parameter. The starting number will always be less than or equal to the ending number. Your function must use recursion by calling itself and not use loops of any kind. It should also work for cases where both startNum and endNum are the same.
+
+function rangeOfNumbers(startNum, endNum) {
+    if (startNum === endNum) {
+        return [startNum];
+    }
+    return rangeOfNumbers(startNum + 1, endNum).concat(rangeOfNumbers(startNum, startNum + 1));
+}
+userNameTwo = "John"
+userAgeTwo = 30
+const user = {name: userNameTwo, age: userAgeTwo}
+
+
+console.log(userNameTwo);
+console.log(userAgeTwo);
+console.log(user)
